062ababdac4bb4fe56ea54694cf134596dc5fe5e
uuzor <victor.u.ezealor@gmail.com>
2023-10-21 03:36:01 +0200

    removed sqlite

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-pip install -r requirements.txt
+
+
+python.exe -m pip install --upgrade pip
+
+python.exe -m pip install -r requirements.txt
 
 python3.9 manage.py collectstatic
--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
